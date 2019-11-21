287d54756424d8c6a6169f97b18e371ac5d8aff5
prostain <peterson.rostain@gmail.com>
2019-11-21 16:03:27 +0100

    premier commit

---
+++ tp-vuejs/js/script.js
@@ -0,0 +1,18 @@
+/**
+ *
+ * display navbar
+ *
+ */
+function myFunction()
+{
+
+    let x = document.getElementById("myLinks");
+    if (x.style.display === "flex" ) {
+        x.style.display = "none";
+    } else {
+        x.style.display = "flex";
+    }
+
+}
+
+
